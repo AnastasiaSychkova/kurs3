41222e50752c68e994f59a78bcabee55811a3a19
Anastasia <sychkova.nastyusha@mail.ru>
2023-04-19 20:29:33 +0400

    hw

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,2 +1,44 @@
-package ru.hogwarts.school.service;public class FacultyService {
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
+
+import java.util.*;
+
+@Service
+public class FacultyService {
+    private Map<Long, Faculty> facultyMap = new HashMap<>();
+    private Long generatedFacultyId = 0L;
+
+    public Faculty createFaculty(Faculty faculty){
+        faculty.setId(generatedFacultyId++);
+        facultyMap.put(faculty.getId(), faculty);
+        return faculty;
+    }
+
+    public Faculty getFacultyById(Long id){
+        return facultyMap.get(id);
+    }
+    public Collection<Faculty> getAllFacultyByColour(String colour){
+        List<Faculty> faculties = new ArrayList<>();
+        for (Faculty faculty : facultyMap.values()){
+            if(faculty.getColor().equals(colour)){
+                faculties.add(faculty);
+            }
+        }
+        return faculties;
+    }
+
+    public Faculty updateFaculty(Faculty faculty){
+        if (!facultyMap.containsKey(faculty.getId())) {
+            return null;
+        }
+        facultyMap.put(faculty.getId(), faculty);
+        return faculty;
+    }
+
+    public Faculty deleteFaculty(Long id){
+        return facultyMap.remove(id);
+    }
 }
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,2 +1,45 @@
-package ru.hogwarts.school.service;public class StudentService {
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
+
+import java.util.*;
+
+@Service
+public class StudentService {
+    private Map<Long, Student> studentMap = new HashMap<>();
+    private Long generatedStudentId = 0L;
+
+    public Student createStudent(Student student) {
+        student.setId(generatedStudentId++);
+        studentMap.put(student.getId(), student);
+        return student;
+    }
+
+    public Student getStudentById(Long id) {
+        return studentMap.get(id);
+    }
+
+    public Collection<Student> getAllStudentByAge(int age) {
+        List<Student> students = new ArrayList<>();
+        for (Student student : studentMap.values()) {
+            if (student.getAge() == age) {
+                students.add(student);
+            }
+        }
+        return students;
+    }
+
+    public Student updateStudent(Student student) {
+        if (!studentMap.containsKey(student.getId())) {
+            return null;
+        }
+        studentMap.put(student.getId(), student);
+        return student;
+    }
+
+    public Student deleteStudent(Long id) {
+        return studentMap.remove(id);
+    }
 }
